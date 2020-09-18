c83b9fd8ec05a9a2f18ea5bd20f5f4c7cb8aab68
Oliver Liao <rtw0913@gmail.com>
2020-09-18 16:02:56 +0800

    clean code

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -16,6 +16,7 @@ export class BarComponent implements OnInit {
 
   private svg: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
   private g: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
+  private tooltip: d3.Selection<d3.BaseType, unknown, HTMLElement, any>;
   private x: d3.ScaleTime<number, number>
   private y: d3.ScaleBand<string>;
   private xAxis: d3.Axis<any>;
@@ -26,7 +27,7 @@ export class BarComponent implements OnInit {
   private spanX: (d: any) => number = (d) => this.x(this.parser(d.dates.start))
   private spanW: (d: any) => number = (d) => this.x(this.parser(d.dates.end)) - this.x(this.parser(d.dates.start))
   private parser: (dateString: string) => Date = d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")
-  private tooltip;
+  private drag;
   
   private isFirst: boolean = true;
   private size: { [key: string]: number } = {
@@ -35,7 +36,6 @@ export class BarComponent implements OnInit {
     height: 300
   }
   
-
   constructor(
     public _gantt: GanttService,
     public _source: DataSourceService
@@ -52,7 +52,6 @@ export class BarComponent implements OnInit {
     if (!dataSet) return;
 
     const array = this.handleRecursive(dataSet);
-
     this.size.height = array.length * 40
 
     if (this.isFirst) {
@@ -82,7 +81,7 @@ export class BarComponent implements OnInit {
 
     this.x = d3.scaleTime()
       .domain([new Date(2020, 2, 1, 0o0, 0o0), new Date(2020, 10, 30, 23, 59)])
-      .range([0, this.size.width + (this.size.margin * 2)]);
+      .range([0, this.size.width + (this.size.margin * 2)]);    
     
     this.xAxis = d3.axisTop(this.x).tickSize(null)
 
@@ -91,14 +90,6 @@ export class BarComponent implements OnInit {
 
     this.grid(this.gGrid, this.x)
     
-    this.zoom = d3.zoom()
-    .scaleExtent([1, 160])
-    .translateExtent([[0, 0], [this.size.width+ (this.size.margin * 2), this.size.height]])
-    .extent([[0, 0], [this.size.width+ (this.size.margin * 2), this.size.height]])
-    .on("zoom", function (event) {
-      _this.zoomed(event)
-    });
-    
     this.g.append("g")
       .attr('class', 'axis axis--x')
       .attr("transform", "translate(0, -8)")
@@ -106,31 +97,22 @@ export class BarComponent implements OnInit {
       .call(this.xAxis)
       .call(g => g.select(".domain").remove())
     
-    this.svg.call(this.zoom)
-    this.drawBars(data)
-  }
-
-  public zoomed(event) {
-    var t = event.transform, xt = t.rescaleX(this.x)
-    this.g.select(".axis--x")
-      .call(this.xAxis.scale(xt))
-      .call(g => g.select(".domain").remove())
-    this.gGrid.call(this.grid, xt, this)
-    this.svg.selectAll("rect")
-      .attr("x", (d) => t.applyX(this.spanX(d)))
-      .attr("width", (d) => t.k * this.spanW(d))
+    this.makeZoom();
+    this.svg.call(this.zoom);
+    this.drawBars(data);
   }
 
   public grid(g, x, component = null) {
     const _this = this === null ? component : this;
-    
-    g.call(g => g
-      .selectAll(".x")
+    const setOrder = d3.descending;
+    const xGrid = d3.selectAll('.x');
+
+    g.call(g => g.selectAll(".x")
       .data(x.ticks())
       .join(
         enter => enter.append("line")
           .attr("stroke-opacity", 1)
-          .attr("stroke", "black")
+          .attr("stroke", "white")
           .attr("class", "x")
           .attr("y2", _this.size.height),
         update => update,
@@ -138,43 +120,14 @@ export class BarComponent implements OnInit {
       )
       .attr("x1", d => x(d))
       .attr("x2", d => x(d)))
-  }
-
-  private makeTooltip(dataSet): void {
-    this.tooltip = d3.select("#bar")
-    .append("div")
-      .style("position", "absolute")
-      .style("background-color", "white")
-      .style("border", "solid")
-      .style("border-width", "1px")
-      .style("border-radius", "5px")
-      .style("padding", "10px")
-      .html(`
-        <p>${dataSet.code}</p>
-        <p>Type: ${dataSet.type}</p>
-        <p>Lanuch period: ${this.timeFormat(dataSet.dates.start)} ~ ${this.timeFormat(dataSet.dates.end)}</p>
-      `);
-  }
-
-  public timeFormat(time): string {
-    const timeAll = new Date(time)
-    let Y, m, d, hh, mm;
-    Y = timeAll.getFullYear();
-    m = timeAll.getMonth()+1;
-    d = timeAll.getDate()
-    hh = timeAll.getHours()-8
-    mm = timeAll.getMinutes();
-    return `${Y}-${this.addZero(m)}-${this.addZero(d)} ${this.addZero(hh)}:${this.addZero(mm)}`
-  }
-
-  public addZero(value): string {
-    return value < 10 ? `0${value}`: value
+    
+    // 排序 svg 元素做 z-index
+    xGrid.sort(setOrder)
   }
 
   private drawBars(data: any[]): void {
     const _this = this;
 
-    // Create and fill the bars
     this.update = this.g.selectAll('rect').data(data);
     this.enter = this.update.enter();
     let exit = this.update.exit();
@@ -214,8 +167,62 @@ export class BarComponent implements OnInit {
     exit.remove();
   }
 
+// --------------------- 縮放行為 ------------------- //
+  private makeZoom(): void {
+    const _this = this;
 
+    this.zoom = d3.zoom()
+    .scaleExtent([1, 160])
+    .translateExtent([[0, 0], [this.size.width+ (this.size.margin * 2), this.size.height]])
+    .extent([[0, 0], [this.size.width+ (this.size.margin * 2), this.size.height]])
+    .on("zoom", function (event) {
+      _this.zoomed(event)
+    });
+  }
 
+  public zoomed(event) {
+    var t = event.transform, xt = t.rescaleX(this.x)
+
+    this.gGrid.call(this.grid, xt, this)
+    this.g.select(".axis--x")
+      .call(this.xAxis.scale(xt))
+      .call(g => g.select(".domain").remove())
+    this.svg.selectAll("rect")
+      .attr("x", (d) => t.applyX(this.spanX(d)))
+      .attr("width", (d) => t.k * this.spanW(d))
+  }
+// --------------------- 提示框 ------------------- //
+  private makeTooltip(dataSet): void {
+    this.tooltip = d3.select("#bar")
+    .append("div")
+      .attr('class', 'tooltip')
+      .html(`
+        <p>${dataSet.code}</p>
+        <p>Type: ${dataSet.type}</p>
+        <p>Lanuch period: ${this.timeFormat(dataSet.dates.start)} ~ ${this.timeFormat(dataSet.dates.end)}</p>
+        <p>Progress: ${dataSet.progress}%</p>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" style="width: ${dataSet.progress}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+      `);
+  }
+
+// --------------------- 時間格式 ------------------- //
+  public timeFormat(time): string {
+    const timeAll = new Date(time)
+    let Y, m, d, hh, mm;
+    Y = timeAll.getFullYear();
+    m = timeAll.getMonth()+1;
+    d = timeAll.getDate()
+    hh = timeAll.getHours()-8
+    mm = timeAll.getMinutes();
+    return `${Y}-${this.addZero(m)}-${this.addZero(d)} ${this.addZero(hh)}:${this.addZero(mm)}`
+  }
+
+  public addZero(value): string {
+    return value < 10 ? `0${value}`: value
+  }
+  
 // --------------------- 資料整理 ------------------- //
   public handleRecursive(dataSet) {
     const array = []
